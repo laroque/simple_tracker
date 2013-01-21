b2e8911dc44ea7e55562be067f133354ed051600
Ben LaRoque <benjamin.laroque@gmail.com>
2013-01-21 15:27:04 -0800

    Starting my own executable.

---
+++ tracker_aspen.cpp
@@ -0,0 +1,11 @@
+/*
+  Taking some basics from Ben M's tracker_3ds6.C and going to try and explore things in prep for Aspen's 2013 "New Directions in Neutrino Physics" conference....
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    printf("I don't do anything yet!\nThanks for playing.\n");
+    return 0;
+}
